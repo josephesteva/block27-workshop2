011466fc8a315e9f1d7e85c442e89547907e9ebc
josephesteva <joe.esteva@gmail.com>
2023-11-17 16:45:02 -0500

    Implement Authenticate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import SignUpForm from './components/SignUpForm'
 import Authenticate from './components/Authenticate'
+import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+	const [token, setToken] = useState(null);
 
   return (
     <>
-			<SignUpForm/>
-			<Authenticate/>
+			<SignUpForm setToken={setToken} />
+			<Authenticate token={token}/>
 		</>
 
   )
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const SignUpForm = () => {
+const SignUpForm = ({setToken}) => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [error, setError] = useState(null);
@@ -20,7 +20,7 @@ const SignUpForm = () => {
 					})
 				});
 			const result = await response.json();
-			console.log(result)
+			setToken(result.token);
 
 		} catch (error) {
 			setError(error.message)
